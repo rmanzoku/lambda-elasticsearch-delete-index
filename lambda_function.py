a24420b3392a50730ba2c5c7109f36faedbf607f
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-10-12 07:24:17 +0000

    Load indices

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python2.6
+import os
+import urllib.request
+import csv
+import io
 
 
 def lambda_handler(event, context):
-    print("Hello lambda")
+
+    endpoint = os.environ.get('ES_ENDPOINT')
+
+    url = endpoint + "/_cat/indices"
+    res = urllib.request.urlopen(url)
+    cr = csv.reader(io.TextIOWrapper(res), delimiter=" ", skipinitialspace=True)
+
+    indices = [c[2] for c in cr]
+
+    print(indices)
     return "end"
