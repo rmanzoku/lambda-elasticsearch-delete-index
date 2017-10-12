f982fc08536cb268c755412e3e93d81f62376d66
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-10-12 08:07:35 +0000

    DELETE indices

--- lambda_function.py
+++ lambda_function.py
@@ -16,19 +16,23 @@ def lambda_handler(event, context):
     keep_suffix = tuple((today - datetime.timedelta(days=o)).isoformat()
                         for o in range(0, older_than))
 
-    url = endpoint + "/_cat/indices"
-    res = urllib.request.urlopen(url)
+    indices_url = endpoint + "/_cat/indices"
+    res = urllib.request.urlopen(indices_url)
     cr = csv.reader(io.TextIOWrapper(res), delimiter=" ", skipinitialspace=True)
 
     indices = [c[2] for c in cr]
     delete_indices = []
 
+    METHOD = "DELETE"
     for i in indices:
         if i.startswith("."):
             continue
 
         if not i.endswith(keep_suffix):
             delete_indices.append(i)
+            request = urllib.request.Request(endpoint+"/"+i)
+            request.get_method = lambda: METHOD
+            response = urllib.request.urlopen(request)
+            print(METHOD, endpoint+"/"+i, response.read().decode('utf-8'))
 
-    print(delete_indices)
     return "end"
